9cd536b4d553beb4aa185d35c1aa9366d553c906
AxdaTFK <axldug5@gmail.com>
2026-08-30 02:03:10 -0500

    aaaa

--- src/Resultados.js
+++ src/Resultados.js
@@ -8,12 +8,13 @@ function AverageLatency(results) {
     }, 0);
     return totalLatency / results.length;
 }
-//////Latencia es mayor al límite indicado (Umbral definido =180) 
+//////Latencia es mayor al límite indicado (Umbral definido = 50) 
 function getEventsAboveDeviation(results, threshold = 50) {
     return results.filter((result) => {
         return (result.realTime - result.Stime) > threshold;
     });
 }
+
 //////Eventos fuera de orden
 function getOutOfOrderEvents(results) {
     let maxTime = -Infinity;
